57aa74e7df56555604389930ea8625e4d0fa817b
unknown <developer@ES00215.esendex.com>
2014-10-25 13:50:22 +0100

    ES-1: Server can receive tag events.

---
+++ Server/RfidSensor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Phidgets;
+using Server.Events;
+
+namespace Server
+{
+    public class RfidSensor : IDisposable
+    {
+        public event EventHandler<TagReceivedEvent> TagReceived;
+
+        private readonly RFID _scanner;
+
+        public RfidSensor()
+        {
+            _scanner = new RFID();
+
+            _scanner.Tag += _scanner_Tag;
+
+            _scanner.open();
+            _scanner.waitForAttachment(2000);
+
+            _scanner.Antenna = true;
+            _scanner.LED = true;
+        }
+
+        void _scanner_Tag(object sender, Phidgets.Events.TagEventArgs e)
+        {
+            var tagData = e.Tag;
+            TagReceived(this, new TagReceivedEvent(tagData));
+        }
+
+        public void Dispose()
+        {
+            _scanner.Antenna = false;
+            _scanner.LED = false;
+        }
+    }
+}
