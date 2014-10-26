908997f06f50deafde2f3c2b2f18aa11937b8b25
unknown <developer@ES00215.esendex.com>
2014-10-26 09:42:47 +0000

    ReadPrefs: Present user with audio file for preferred stops.

---
+++ Server/Services/UserPreferenceService.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Server.Services
+{
+    public class UserPreferenceService
+    {
+        private readonly List<BusRoutePreference> _fakeUserPreferences;
+
+        public UserPreferenceService()
+        {
+            _fakeUserPreferences = new List<BusRoutePreference>
+                {
+                    new BusRoutePreference
+                        {
+                            userTagId = "110057f2cd",
+                            Routes = new List<BusRoute>
+                                {
+                                    new BusRoute
+                                        {
+                                            RouteNumber = "2",
+                                            Direction = "Shudehill Interchange (Shudehill)"
+                                        },
+                                    new BusRoute
+                                        {
+                                            RouteNumber = "3",
+                                            Direction = "Spinningfields, Quay Street/Deansgate"
+                                        }
+                                }
+                        }
+                };
+        }
+
+        public List<BusRoute> GetBusRoutesPreferencesForTag(string tagId)
+        {
+            var routePreferencesForUser = _fakeUserPreferences.Single(userPref => userPref.userTagId == tagId);
+            return routePreferencesForUser.Routes;
+        }
+    }
+
+    public class BusRoutePreference
+    {
+        public string userTagId { get; set; }
+        public List<BusRoute> Routes { get; set; }
+    }
+
+    public class BusRoute
+    {
+        public string RouteNumber { get; set; }
+        public string Direction { get; set; }
+    }
+}
